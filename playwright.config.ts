d653dda6543535be25c4c312dd5b68fa2403f497
idembele70 <idembele70@gmail>
2025-03-08 08:02:20 +0100

    chore: increase workers to three, one per project

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests. */
-  workers: 1,
+  /* 3 workers because there's 3 projects */
+  workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
